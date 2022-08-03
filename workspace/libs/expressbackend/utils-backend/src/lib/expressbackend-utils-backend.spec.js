4cb652d4f1825822968b11281616f6f6977719cc
Karthik <jsdevkarthik@gmail.com>
2022-08-03 20:42:13 +0530

    Removed empty folders. Nx Workspace Setup Complete

---
+++ workspace/libs/expressbackend/utils-backend/src/lib/expressbackend-utils-backend.spec.js
@@ -0,0 +1,8 @@
+import { expressbackendUtilsBackend } from './expressbackend-utils-backend';
+describe('expressbackendUtilsBackend', () => {
+  it('should work', () => {
+    expect(expressbackendUtilsBackend()).toEqual(
+      'expressbackend-utils-backend'
+    );
+  });
+});
